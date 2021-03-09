8a4f36e0a9c73c1f84b9080292562971c0e02a41
Stefan Diskic <disko998@gmail.com>
2021-03-09 15:32:50 +0100

    fetch package versions

--- scripts/integrity.js
+++ scripts/integrity.js
@@ -1,100 +1,29 @@
-#!/usr/bin/env node
+"use strict";
+const axios = require("axios").default;
 
-'use strict';
+const baseURL = "https://data.jsdelivr.com/v1/package/npm";
 
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-const { files } = require('../config');
-const { generateSri } = require('../lib/helpers');
-
-const configFile = path.resolve(__dirname, '../config/_files.yml');
-
-function buildPath(dir) {
-    dir = dir.replace('/bootstrap/', '/twitter-bootstrap/')
-        .replace('https://stackpath.bootstrapcdn.com/', '');
-
-    return path.join(__dirname, '../cdn', dir);
+async function getPackage(packageName) {
+    const { data } = await axios.get(`${baseURL}/${packageName}`);
+    return data;
 }
 
-function exists(file) {
-    const found = fs.existsSync(file);
+async function onPackageVersions({ versions }) {
+    try {
+        const promises = versions.map(async (version) => {
+            return await getPackage(`bootstrap@${version}`);
+        });
 
-    if (!found) {
-        console.warn(`WARNING: ${file} not found`);
+        const packages = await Promise.all(promises);
+
+        console.log(packages);
+    } catch (error) {
+        console.error(error);
     }
-
-    return found;
 }
 
-// Bootswatch {3,4}
-['bootswatch3', 'bootswatch4'].forEach((key) => {
-    const bootswatch = buildPath(files[key].bootstrap);
-
-    files[key].themes.forEach((theme) => {
-        const file = bootswatch.replace('SWATCH_VERSION', files[key].version)
-            .replace('SWATCH_NAME', theme.name);
-
-        if (exists(file)) {
-            theme.sri = generateSri(file);
-        }
+getPackage("bootstrap")
+    .then(onPackageVersions)
+    .catch((err) => {
+        console.log(err.message);
     });
-});
-
-// Bootlint
-files.bootlint.forEach((bootlint) => {
-    const file = buildPath(bootlint.javascript);
-
-    if (exists(file)) {
-        bootlint.javascriptSri = generateSri(file);
-    }
-});
-
-// Bootstrap
-files.bootstrap.forEach((bootstrap) => {
-    const javascript = buildPath(bootstrap.javascript);
-    const stylesheet = buildPath(bootstrap.stylesheet);
-    let { javascriptBundle, javascriptEsm } = bootstrap;
-
-    if (javascriptBundle) {
-        javascriptBundle = buildPath(bootstrap.javascriptBundle);
-    }
-
-    if (javascriptEsm) {
-        javascriptEsm = buildPath(bootstrap.javascriptEsm);
-    }
-
-    if (exists(javascript)) {
-        bootstrap.javascriptSri = generateSri(javascript);
-    }
-
-    if (javascriptBundle && exists(javascriptBundle)) {
-        bootstrap.javascriptBundleSri = generateSri(javascriptBundle);
-    }
-
-    if (javascriptEsm && exists(javascriptEsm)) {
-        bootstrap.javascriptEsmSri = generateSri(javascriptEsm);
-    }
-
-    if (exists(stylesheet)) {
-        bootstrap.stylesheetSri = generateSri(stylesheet);
-    }
-});
-
-// Font Awesome
-files.fontawesome.forEach((fontawesome) => {
-    const stylesheet = buildPath(fontawesome.stylesheet);
-
-    if (exists(stylesheet)) {
-        fontawesome.stylesheetSri = generateSri(stylesheet);
-    }
-});
-
-// Create backup file
-fs.copyFileSync(configFile, `${configFile}.bak`);
-
-fs.writeFileSync(configFile,
-    yaml.dump(files, {
-        lineWidth: -1
-    })
-);
